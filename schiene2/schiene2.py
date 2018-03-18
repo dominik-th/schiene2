9d6172fbf6917382a90fdc5ca504bcb7dece3869
Dominik Thiemermann <dominik@dthiemermann.org>
2018-03-18 19:25:53 +0100

    Add method to query train stations eva id from db api

--- schiene2/schiene2.py
+++ schiene2/schiene2.py
@@ -1,6 +1,18 @@
+import requests
+import xml.etree.ElementTree as ET
 from datetime import datetime
 
 class Schiene2():
+
+  dbAccessToken = '3356e54605aa647bc8cb8c4eb70a1938'
+
+  def eva(self, station):
+    headers = {
+      'Authorization': 'Bearer {0}'.format(self.dbAccessToken)
+    }
+    res = requests.get('https://api.deutschebahn.com/timetables/v1/station/{0}'.format(station), headers=headers)
+    root = ET.fromstring(res.content)
+    return root[0].attrib['eva']
 
   def connections(self, origin, destination, dt=datetime.now(), only_direct=False):
     return
