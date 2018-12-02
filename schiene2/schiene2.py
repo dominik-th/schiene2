d2750a79ae4a45c99aaa8d9265e695e73113bf39
Dominik Thiemermann <dominik@dthiemermann.org>
2018-12-02 22:47:08 +0100

    Remove db eva api

--- schiene2/schiene2.py
+++ schiene2/schiene2.py
@@ -21,16 +21,6 @@ def parse_stations(html):
 
 class Schiene2():
 
-  dbAccessToken = '3356e54605aa647bc8cb8c4eb70a1938'
-
-  def evaDBApi(self, station):
-    headers = {
-      'Authorization': 'Bearer {0}'.format(self.dbAccessToken)
-    }
-    res = requests.get('https://api.deutschebahn.com/timetables/v1/station/{0}'.format(station), headers=headers)
-    root = ET.fromstring(res.content)
-    return root[0].attrib['eva']
-
   def eva(self, station):
     """
     Find EVA-number of a station by its name
